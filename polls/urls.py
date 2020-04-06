c0e56fcd99ed137dd37f43cfa6cd9d936519227f
MacBook Pro <enablis.senegal@gmail.com>
2020-04-06 06:58:04 +0000

    forst commit

--- polls/urls.py
+++ polls/urls.py
@@ -7,11 +7,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/<int:postProp_id>/', views.detail, name='detail'),
-    path('/passerannonce', views.passer, name='passer'),
-    path('/passannonceinsertion', views.passerAjouter, name='passerannonceinsertion'),
-    path('/login', views.loginform, name='login'),
-    path('/loginprocess', views.loginprocess, name='loginprocess'),
-    path('/logout', views.logout_view, name="logout"),
-    path('/list', views.list, name='list'),
+    path('<int:postProp_id>/', views.detail, name='detail'),
+    path('passerannonce', views.passer, name='passer'),
+    path('passannonceinsertion', views.passerAjouter, name='passerannonceinsertion'),
+    path('login', views.loginform, name='login'),
+    path('loginprocess', views.loginprocess, name='loginprocess'),
+    path('logout', views.logout_view, name="logout"),
+    path('list', views.list, name='list'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
